f190bdef7b9356ca0b00e845e95aea42500a3a44
Priyanshu Mohite <priyanshu.mohite07@gmail.com>
2026-08-11 19:24:55 +0530

    added backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     try {
       // Backend ko API call
-      const response = await fetch('http://127.0.0.1:8000/chat', {
+      const response = await fetch('https://ai-portfolio-chatbot-0cqn.onrender.com/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ user_input: input })
